1ce60f1c29e856b15b9f7e4b0568fc91f3169aa0
N0bodys-here <nobodyishere1111111a@gmail.com>
2026-04-20 11:47:09 -0400

    Update scene text and choices in part1.js

--- part1.js
+++ part1.js
@@ -1,13 +1,56 @@
 const scenes = {
-  sceneStart: {
-    text: "Start menu",
+ sceneStart: {
+    text: ` <span style="color:;">Hello</span> player <span style="color:;">welcome to the experience. First you’ll have to do a few things before you can begin but these choices should help make your experience much more enjoyable and personalized. You may continue.</span>`,
     choices: [
       {
-        text: "start",
-        next: "scene1S",
+        text: "Continue",
+        next: "start1",
       },
     ]
   },
+
+start1: {
+    text: `<span style="color:blue;">Please choose your socially constructed sets of personality roles that will be assigned to you. This will decide how people refer to you as in pronouns, honorifics, and more. This will not decide your actions, you may decide your actions.  Note: this may affect specific outcomes or events in your experience.</span>`,
+    choices: [
+      {
+        text: "Masculine roles",
+        next: "start2",
+        Effect: {gameState.playerGender: `masculine`}
+      },
+     {
+        text: "Feminine roles",
+        next: "start2",
+        Effect: {gameState.playerGender: `feminine`}
+      },
+     {
+        text: "Neutral roles",
+        next: "start2",
+        Effect: {gameState.playerGender: `neutral`}
+      },
+
+    ]
+  },
+
+start2: {
+    text: `<span style="color:;">${p(monarch)} detected… Now, please tell me the name you would like to be referred to or choose to have a name chosen for you. Please be aware others in the experience will not react to any unusual names you decide to call yourself thanks to the system but you will. So keep in mind if you want an enjoyable experience I suggest choosing a normal sounding name, whatever that may be for you.</span> `,
+    choices: [
+      {
+        text: "Chosen Name",
+        next: "start3",
+      },
+    ]
+  },
+
+start3: {
+    text: `<span style="color:;">Good luck</span> player<span style="color:;">, i will follow you in your experience but hopefully not interfere. Now, you may begin.</span>`,
+    choices: [
+      {
+        text: "Begin",
+        next: "sceneS1",
+      },
+    ]
+  },
+
   scene1S: {
     text: "Someone drops their books in front of you.",
     choices: [

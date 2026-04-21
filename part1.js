d7f0f7bca58d95d246eecc61af3695dae030bc68
N0bodys-here <nobodyishere1111111a@gmail.com>
2026-04-21 15:40:34 -0400

    Fix variable name from gamestate to gameState

--- part1.js
+++ part1.js
@@ -51,7 +51,7 @@ start2: {
   },
 
 start3: {
-    text: () => `<span style="color: lightblue;">Name Chosen: ${gamestate.socialRoles}. Good luck</span> player<span style="color: lightblue;">, i will follow you in your experience but hopefully not interfere. Now, you may begin.</span>`,
+    text: () => `<span style="color: lightblue;">Name Chosen: ${gameState.socialRoles}. Good luck</span> player<span style="color: lightblue;">, i will follow you in your experience but hopefully not interfere. Now, you may begin.</span>`,
     choices: [
       {
         text: "Begin",
